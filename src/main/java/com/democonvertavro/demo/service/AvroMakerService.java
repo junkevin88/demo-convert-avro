67a4e275cba0baed7e95955a6a57dcefee6f7dcd
Jun Kevin <junkevin88@gmail.com>
2024-04-10 08:26:32 +0700

    fix: clean up comments

--- src/main/java/com/democonvertavro/demo/service/AvroMakerService.java
+++ src/main/java/com/democonvertavro/demo/service/AvroMakerService.java
@@ -8,7 +8,6 @@ import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -34,26 +33,6 @@ public class AvroMakerService {
         }
         Schema schema = new Schema.Parser().parse(schemaInputStream);
         schemaInputStream.close();
-
-        // Create Avro data file
-//        try (DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<>(new GenericDatumWriter<>(schema))) {
-//            dataFileWriter.create(schema, new File("users.avro"));
-//
-//            // Create sample records
-//            GenericRecord user1 = new GenericData.Record(schema);
-//            user1.put("name", "John");
-//            user1.put("city", "Jakarta");
-//            user1.put("code", 123);
-//
-//            GenericRecord user2 = new GenericData.Record(schema);
-//            user2.put("name", "Alice");
-//            user2.put("city", "Surabaya");
-//            user2.put("code", 456);
-//
-//            // Write records to Avro file
-//            dataFileWriter.append(user1);
-//            dataFileWriter.append(user2);
-//        }
 
         try (DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<>(new GenericDatumWriter<>(schema))) {
             dataFileWriter.create(schema, new File("users.avro"));
